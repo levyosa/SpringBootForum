5475842ff5e80e74db09b1524efc9c5ae0bcbca2
Levy Ferreira <levyjorg@gmail.com>
2021-10-15 17:01:49 -0300

    create TopicDTO so not all the data is returned on the api call

---
+++ src/main/java/com/example/forum/controller/dto/TopicDTO.java
@@ -0,0 +1,42 @@
+package com.example.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.forum.model.Topic;
+
+public class TopicDTO {
+    private Long id;
+    private String title;
+    private String message;
+    private LocalDateTime creationDate;
+
+    public TopicDTO(Topic topic){
+        this.id = topic.getId();
+        this.title = topic.getTitle();
+        this.message = topic.getMessage();
+        this.creationDate = topic.getCreationDate();
+
+    }
+
+    public static List<TopicDTO> convert(List<Topic> topics) {
+        return topics.stream().map(TopicDTO::new).collect(Collectors.toList());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+}
