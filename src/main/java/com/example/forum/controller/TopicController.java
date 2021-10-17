10d0a099e99dc22d286bbc71f48869113aa3437a
Levy Ferreira <levyjorg@gmail.com>
2021-10-17 11:33:58 -0300

    Added Repository Dependency injection instead of hard coded data returning

--- src/main/java/com/example/forum/controller/TopicController.java
+++ src/main/java/com/example/forum/controller/TopicController.java
@@ -1,12 +1,9 @@
 package com.example.forum.controller;
-
-
-import java.util.Arrays;
 import java.util.List;
-
+import com.example.forum.TopicRepository;
 import com.example.forum.controller.dto.TopicDTO;
-import com.example.forum.model.Course;
 import com.example.forum.model.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
     @RequestMapping("/topics")
-    public List<TopicDTO> lista (){
-        Topic topic = new Topic("Dúvida","duvida com spring",new Course("Spring","Programacao"));
-        return TopicDTO.convert(Arrays.asList(topic,topic,topic,topic));
+    public List<TopicDTO> lista (String courseName){
+        List<Topic> topics = topicRepository.findAll();
+        return TopicDTO.convert(topics);
 
     }
+
+
+
+
+
+    @Autowired
+    private TopicRepository topicRepository;
 }
