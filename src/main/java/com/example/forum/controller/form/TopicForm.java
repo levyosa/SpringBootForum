d3799755e2b6137127b544de7cb665e765608ad5
Levy Ferreira <levyjorg@gmail.com>
2021-10-17 18:26:07 -0300

    Annotates TopicForm so it can be validated by Spring (BeanValidation)

--- src/main/java/com/example/forum/controller/form/TopicForm.java
+++ src/main/java/com/example/forum/controller/form/TopicForm.java
@@ -1,12 +1,20 @@
 package com.example.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import com.example.forum.model.Course;
 import com.example.forum.model.Topic;
 import com.example.forum.repository.CourseRepository;
+import org.hibernate.validator.constraints.Length;
+
 
 public class TopicForm {
+    @NotNull @NotEmpty @Length(min = 5)
     private String title;
+    @NotNull @NotEmpty @Length(min = 10)
     private String message;
+    @NotNull @NotEmpty
     private String courseName;
 
     public String getTitle() {
